ce24e6ee1d790cdde3a11120c42ca3b3312df15d
Swoyam <swoyam@gmail.com>
2025-07-23 20:24:18 +0545

    install core and backend update

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import dotenv from "dotenv"
 import route from "./Routes/userRoutes.js"
+import cors from "cors"
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
 dotenv.config();
 
 const PORT = process.env.PORT || 7000;
